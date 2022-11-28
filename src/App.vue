61cb375ae6babecf8354fb410bf5b289a38e7304
thompson <alfredthompsonovie@gmail.com>
2022-11-29 00:16:54 +0100

    add images, data.json, top bg css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
+  <div class="top-bg"></div>
   <div>
     job listings
+  </div>
+  <div class="attribution" v-if="0">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">Your Name Here</a>.
   </div>
 </template>
 
